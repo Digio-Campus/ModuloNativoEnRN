f68ae6634952cedd96124aec1581bb95f7b449e3
Victor <elsodah12@gmail.com>
2024-03-24 12:14:36 +0100

    Uso e integracion de modulo nativo de Android en RN

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { NativeModules } from 'react-native';
+
+const { CalendarModule } = NativeModules;
+const funcionBoton = () => {
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+};
+
+export default funcionBoton;
 
 AppRegistry.registerComponent(appName, () => App);
